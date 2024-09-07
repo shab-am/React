4168d9cb848e63a7aa2a594fe012f22cf6f20c90
shab-am <shabnam.hoypuc1@gmail.com>
2024-09-07 19:14:43 +0530

    Necessary changes

--- counterExample/src/App.jsx
+++ counterExample/src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  let [count, setCount] = useState(1)
+  let [count, setCount] = useState(1);
   const add = () => {
-    setCount(count + 1)
-    console.log('clicked', count)
-  }
+    if (count < 20) setCount(count + 1);
+  };
   const subtract = () => {
-    setCount(count - 1)
-  }
+    if (count > 0) setCount(count - 1);
+  };
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={add}>Add value by 1</button>
-      <button onClick={subtract}>Reduce value by 1</button>
+      <button onClick={add}>{count} + 1</button>
+      <button onClick={subtract}>{count} - 1</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
